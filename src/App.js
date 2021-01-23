e9be2090fb285500bb9827977f53e554e4705519
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2021-01-23 15:01:31 -0500

    checkpoint

--- src/App.js
+++ src/App.js
@@ -3,33 +3,42 @@ import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import EbayOptions from './components/EbayOptions'
 import ProfitBreakdown from './components/ProfitBreakdown'
+import currency from "currency.js"
 
 function App() {
   const [sellAmount, setSellAmount] = useState(0);
   const [netAmount, setNetAmount] = useState(0);
   const [ebayFeeAmount, setEbayFeeAmount] = useState(0);
   const [payPalFeeAmount, setPayPalFeeAmount] = useState(0);
-  const ebayFee = .1;
-  const paypalFee = .029;
-  const paypalFlatFee = .3;
-  const magicNumber = 1.14811;
+  const ebayFeeRate = .1;
+  const paypalFeeRate = .029;
+  const paypalFlatFeeRate = .3;
+  const constant = 1.14811;
 
-  const calculateSellAmount = () => {
-    const desiredNetAmount = parseInt(document.getElementById('net-earnings').value) || 0;
-    setNetAmount(desiredNetAmount);
+  const calculator = () => {
+    const desiredAmount = (document.getElementById("desiredNetAmount").value || 0)
+    setNetAmount(currency(desiredAmount).value);
+    const newSellAmount = desiredAmount * constant;
+    document.getElementById("itemSoldAmount").value = currency(newSellAmount).value;
+    setSellAmount(currency(newSellAmount).value)
+
+    // const itemCostAmount = document.getElementById("#itemCost").value.length ? document.getElementById("#itemCost").value : 0
+    // const itemSoldAmount = document.getElementById("#itemSoldAmount").value.length ? document.getElementById("#itemSoldAmount").value : 0
+    // const paypalType = document.getElementById("#paypal-checkbox").checked ? "domestic" : false
+
     // const paypalChecked = document.getElementById('paypal-checkbox').checked;
     // const newSellAmount = paypalChecked ? (netAmount + (netAmount * totalFeePercent)) + paypalFlatFee : netEarnings * ebayFee;
-    const newSellAmount = (desiredNetAmount + paypalFlatFee) * magicNumber;
-    setEbayFeeAmount((newSellAmount * ebayFee).toFixed(2))
-    setPayPalFeeAmount(((newSellAmount * paypalFee) + paypalFlatFee).toFixed(2))
-    setSellAmount(newSellAmount.toFixed(2));
+    // const paypalFee
+    // const newSellAmount = desiredNetAmount.add(paypalFlatFee).multiply(constant)
+    // setEbayFeeAmount(newSellAmount.multiply(ebayFee))
+    // setPayPalFeeAmount(((newSellAmount * paypalFee) + paypalFlatFee).toFixed(2))
+    // setSellAmount(newSellAmount.toFixed(2));
   }
   return <Container>
-    <h1 className="display-3 text-center font-weight-bold">Ebay Net Profit</h1>
-    <h1 className="display-3 text-center font-weight-bold mb-4">Calculator 🏷</h1>
+    <h1 className="text-center font-weight-bold mb-4">Ebay Net Profit Calculator 🏷</h1>
     <Row>
       <Col md={4}>
-        <EbayOptions calculateSellAmount={() => calculateSellAmount} />
+        <EbayOptions calculator={() => calculator} />
       </Col>
       <Col>
         <ProfitBreakdown sellAmount={sellAmount} netAmount={netAmount} ebayFeeAmount={ebayFeeAmount} payPalFeeAmount={payPalFeeAmount} />
--- src/components/EbayOptions.js
+++ src/components/EbayOptions.js
@@ -7,28 +7,52 @@ const EbayOptions = (props) => {
             <Card.Body>
                 <Card.Title className="text-center">Ebay Options</Card.Title>
                 <Form>
-                    <Form.Group controlId="net-earnings">
+                    <Form.Group controlId="desiredNetAmount">
                         <Form.Label>Desired Net Earnings</Form.Label>
                         <InputGroup>
                             <InputGroup.Prepend>
-                                <InputGroup.Text id="net-earnings-input">$</InputGroup.Text>
+                                <InputGroup.Text id="desiredNetAmount-input">$</InputGroup.Text>
                             </InputGroup.Prepend>
-                            <Form.Control type="number" placeholder="400" aria-describedby="net-earnings-input" onChange={props.calculateSellAmount()} />
+                            <Form.Control type="number" placeholder="400" aria-describedby="desiredNetAmount-input" onChange={props.calculator()} />
                         </InputGroup>
                         <Form.Text className="text-muted">
                             Net earnings of item sold, after all fees considered.
                     </Form.Text>
                     </Form.Group>
-                    {/* <Form.Check
+                    <Form.Group controlId="itemSoldAmount">
+                        <Form.Label>Sold Amount</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Prepend>
+                                <InputGroup.Text id="itemSoldAmount-input">$</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <Form.Control type="number" placeholder="400" aria-describedby="itemSoldAmount-input" onChange={props.calculator()} />
+                        </InputGroup>
+                        <Form.Text className="text-muted">
+                            Ebay sold amount.
+                    </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="itemCost">
+                        <Form.Label>Item Cost</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Prepend>
+                                <InputGroup.Text id="itemCost-input">$</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <Form.Control type="number" placeholder="400" aria-describedby="itemCost-input" onChange={props.calculator()} />
+                        </InputGroup>
+                        <Form.Text className="text-muted">
+                            Item total cost.
+                    </Form.Text>
+                    </Form.Group>
+                    <Form.Check
                         type={'checkbox'}
                         id={'paypal-checkbox'}
                         label={'Paypal'}
                         defaultChecked
-                        onChange={props.calculateSellAmount()}
+                        onChange={props.calculator()}
                     />
                     <Form.Text className="text-muted">
                         PayPal fees is 2.9% + $0.30 of the total selling price including the sales tax. This fee is calculated before eBay fees.
-                    </Form.Text> */}
+                    </Form.Text>
                 </Form>
             </Card.Body>
         </Card>
